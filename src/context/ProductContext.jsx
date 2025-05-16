7ac173819f3fc3765b0f466a599a838398a442f3
Mobina Karimi <mobina.801407@gmail.com>
2025-05-16 23:50:56 +0330

    Add Card,Category,searchBox,..

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,36 @@
+import { useContext, useEffect } from 'react'
+import { useState } from 'react'
+import { createContext } from 'react'
+import api from '../services/config'
+
+const ProductContext = createContext()
+
+function ProductsProvider({children}) {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async() =>{
+            try {
+                // const response = await api.get("/products")
+                // setProducts(response)
+                setProducts(await api.get("/products"))
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+        fetchProducts()
+    }, [])
+  return (
+    <ProductContext.Provider value={products}>
+        {children}
+    </ProductContext.Provider>
+  )
+}
+
+const useProducts = () => {
+    const products = useContext(ProductContext)
+    return products
+}
+
+export default ProductsProvider
+export {useProducts}
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,78 @@
+import { ImSearch } from "react-icons/im"
+import Card from "../components/Card"
+import Loader from "../components/Loader"
+import { useProducts } from "../context/ProductContext"
+import styles from "./ProductsPage.module.css"
+import { useEffect, useState } from "react"
+import { FaListUl } from "react-icons/fa"
+import { filterProducts, searchProducts } from "../helpers/helper";
+
+function ProductsPage() {
+  const products = useProducts()
+  const [search, setSearch] =useState("")
+  const [displayed, setDisplayed] =useState([])
+  const [query, setQuery] = useState({})
+
+  useEffect(() => {
+    setDisplayed(products)
+  } ,[products])
+
+  useEffect(() => {
+    // console.log(query)
+    let finalProducts = searchProducts(products, query.search)
+    finalProducts = filterProducts(finalProducts, query.category)
+    // console.log(finalProducts)
+    setDisplayed(finalProducts)
+  }, [query])
+
+  const searchHandler = () => {
+    // console.log("search")
+    setQuery(query => ({...query, search}))
+  }
+
+  const categoryHandler = (event) => {
+    const {tagName} = event.target
+    const category = event.target.innerText.toLowerCase()
+
+    if(tagName !== "LI") return
+
+    setQuery(query => ({...query, category}))
+    // console.log(category)
+  }
+  // console.log(products)
+  return (
+    <>
+    <div>
+      <input 
+        type="text" 
+        placeholder="search..." 
+        value={search} 
+        onChange={e => setSearch(e.target.value.toLowerCase().trim())}/>
+      <button onClick={searchHandler}>
+        <ImSearch/>
+      </button>
+    </div>
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!displayed.length && <Loader/>}
+        {displayed.map(p => <Card key={p.id} data={p}/>)}
+      </div>
+      <div>
+        <div>
+          <FaListUl/>
+          <p>Categories</p>
+        </div>
+        <ul onClick={categoryHandler}>
+          <li>All</li>
+          <li>Electronics</li>
+          <li>Jewelery</li>
+          <li>Men's Clothing</li>
+          <li>Women's Clothing</li>
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default ProductsPage
