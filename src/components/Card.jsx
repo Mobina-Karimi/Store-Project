c30b60c7e92ad7a1245ca9b345eb12dca3c318a9
Mobina Karimi <mobina.801407@gmail.com>
2025-05-19 23:28:35 +0330

    Finished actions of reducer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import { TbListDetails, TbShoppingBagCheck } from "react-icons/tb"
 import { Link } from "react-router-dom"
-import {shortenText} from "../helpers/helper"
+import {productQuantity, shortenText} from "../helpers/helper"
 import styles from "./Card.module.css"
 import { useCart } from "../context/CartContext"
+import { MdDeleteOutline } from "react-icons/md"
 
 function Card({data}) {
     const {id, title, image, price} = data
@@ -11,10 +12,13 @@ function Card({data}) {
     // const result = useCart()
     const [state, dispatch] = useCart()
     // console.log(result)
-    console.log(state)
+    // console.log(state)
 
-    const clickHandler = () => {
-        dispatch({type: "ADD_ITEM", payload: data})
+    const quantity = productQuantity(state,id)
+    // console.log(quantity)
+
+    const clickHandler = (type) => {
+        dispatch({type, payload: data})
     }
 
   return (
@@ -27,9 +31,26 @@ function Card({data}) {
                 <TbListDetails/>
             </Link>
             <div>
-                <button onClick={clickHandler}>
+                {quantity == 1 && (
+                    <button onClick={() => clickHandler("REMOVE_ITEM")}>
+                    <MdDeleteOutline/>
+                    </button>
+                )}
+                {quantity > 1 && (
+                     <button onClick={() => clickHandler("DECREASE")}>
+                    -
+                    </button>
+                )}
+                {!!quantity && <span>{quantity}</span>}
+                {quantity == 0 ? (
+                    <button onClick={() => clickHandler("ADD_ITEM")}>
                     <TbShoppingBagCheck/>
-                </button>
+                    </button>) : (
+                    <button onClick={() => clickHandler("INCREASE")}>
+                    +
+                    </button>
+                    )
+                }
             </div>
         </div>
     </div>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -23,6 +23,37 @@ const reducer = (state, action) => {
                 ...sumProducts(state.selectedItems),
                 checkout: false
             }
+        case "REMOVE_ITEM":
+            const newSelectedItem = state.selectedItems.filter(item => item.id !== action.payload.id)
+            return {
+                ...state,
+                selectedItems: [...newSelectedItem],
+                ...sumProducts(newSelectedItem),
+                checkout: false
+            }
+        case "INCREASE":
+            const increaseIndex = state.selectedItems.findIndex(item => item.id == action.payload.id)
+            state.selectedItems[increaseIndex].quantity++
+            return {
+                ...state,
+                ...sumProducts(state.selectedItems),
+                checkout: false
+            }
+        case "DECREASE":
+            const decreaseIndex = state.selectedItems.findIndex(item => item.id == action.payload.id)
+            state.selectedItems[decreaseIndex].quantity--
+            return {
+                ...state,
+                ...sumProducts(state.selectedItems),
+                checkout: false
+            }
+        case "CHECKOUT":
+            return {
+            selectedItems : [],
+            itemsCounter : 0,
+            total: 0,
+            checkout: true
+            }
         default:
             throw new Error("Invalid Action!")
     }
